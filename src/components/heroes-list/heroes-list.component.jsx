bc499f3fb05f2e82bc857dee84d50517bc543456
Strahinja Cokic <55125319+scokic@users.noreply.github.com>
2021-06-12 02:08:45 +0200

    Improved pagination, fixed API call bug, added loading animation
    - Improved pagination
    - Fixed a bug for multiple API calls during session
    - Added loading animation while waiting for API response

--- src/components/heroes-list/heroes-list.component.jsx
+++ src/components/heroes-list/heroes-list.component.jsx
@@ -9,9 +9,11 @@ class HeroesList extends React.Component {
     super(props);
 
     this.state = {
+      loading: true,
       data: "",
       heroes: [],
-      totalPages: 0,
+      totalHeroes: 0,
+      totalPages: 1,
       showOnPage: 24,
       offset: 0,
       page: 1,
@@ -19,21 +21,51 @@ class HeroesList extends React.Component {
 
     this.nextPage = this.nextPage.bind(this);
     this.prevPage = this.prevPage.bind(this);
+    this.firstPage = this.firstPage.bind(this);
+    this.lastPage = this.lastPage.bind(this);
   }
 
   nextPage() {
-    this.setState({
-      page: this.state.page + 1,
-    });
+    this.setState(
+      {
+        page: this.state.page + 1,
+        loading: true,
+      },
+      () => this.callApi(this.state.page)
+    );
   }
 
   prevPage() {
-    this.setState({
-      page: this.state.page - 1,
-    });
+    this.setState(
+      {
+        page: this.state.page - 1,
+        loading: true,
+      },
+      () => this.callApi(this.state.page)
+    );
   }
 
-  async componentDidMount() {
+  firstPage() {
+    this.setState(
+      {
+        page: 1,
+        loading: true,
+      },
+      () => this.callApi(this.state.page)
+    );
+  }
+
+  lastPage() {
+    this.setState(
+      {
+        page: this.state.totalPages,
+        loading: true,
+      },
+      () => this.callApi(this.state.page)
+    );
+  }
+
+  async callApi() {
     const marvelUrl = "https://gateway.marvel.com/v1/public/characters?";
     let ts = Date.now();
     const publicKey = process.env.REACT_APP_PUBLIC_KEY;
@@ -49,40 +81,39 @@ class HeroesList extends React.Component {
     this.setState({
       data: data,
       heroes: data.data.results,
-      totalPages: data.data.total,
+      totalHeroes: data.data.total,
+      totalPages: Math.trunc(data.data.total / showOnPage + 1),
+      loading: false,
     });
   }
 
-  async componentDidUpdate() {
-    const marvelUrl = "https://gateway.marvel.com/v1/public/characters?";
-    let ts = Date.now();
-    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
-    const privateKey = process.env.REACT_APP_PRIVATE_KEY;
-    let hash = MD5(`${ts}${privateKey}${publicKey}`);
-    let offset = this.state.page * 20 - 20;
-    let showOnPage = this.state.showOnPage;
-    let callString = `${marvelUrl}ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=${showOnPage}&offset=${offset}`;
-
-    const response = await fetch(callString);
-    const data = await response.json();
-
-    this.setState({
-      data: data,
-      heroes: data.data.results,
-      totalPages: data.data.total,
-    });
+  async componentDidMount() {
+    this.callApi();
   }
 
   render() {
+    if (this.state.heroes === []) {
+      return (
+        <div className='out-of-calls'>
+          <p>Sorry, we're out of calls for today. 😢</p>
+          <p>Please visit us tomorrow when Marvel allows us to fetch API again!</p>
+        </div>
+      );
+    }
+
     return (
       <div className='hero-grid'>
-        <Pagination page={this.state.page} nextPage={this.nextPage} prevPage={this.prevPage} />
-        <div className='hero-grid-wrapper'>
-          {this.state.heroes.map((hero) => (
-            <HeroCard key={hero.id} name={hero.name} image={hero.thumbnail.path} description={hero.description} series={hero.series.items} />
-          ))}
-        </div>
-        <Pagination page={this.state.page} nextPage={this.nextPage} prevPage={this.prevPage} />
+        <Pagination page={this.state.page} nextPage={this.nextPage} prevPage={this.prevPage} totalPages={this.state.totalPages} firstPage={this.firstPage} lastPage={this.lastPage} />{" "}
+        {this.state.loading ? (
+          <div class='loader'></div>
+        ) : (
+          <div className='hero-grid-wrapper'>
+            {this.state.heroes.map((hero) => (
+              <HeroCard key={hero.id} name={hero.name} image={hero.thumbnail.path} description={hero.description} series={hero.series.items} />
+            ))}{" "}
+          </div>
+        )}
+        <Pagination page={this.state.page} nextPage={this.nextPage} prevPage={this.prevPage} totalPages={this.state.totalPages} firstPage={this.firstPage} lastPage={this.lastPage} />
       </div>
     );
   }
--- src/components/pagination/pagination.component.jsx
+++ src/components/pagination/pagination.component.jsx
@@ -9,17 +9,33 @@ class Pagination extends React.Component {
           {this.props.page === 1 ? (
             ""
           ) : (
-            <button className='prev-page-btn' onClick={this.props.prevPage}>
-              -
-            </button>
+            <>
+              <button className='prev-page-btn' onClick={this.props.prevPage}>
+                -
+              </button>
+              <button className='first-page-btn' onClick={this.props.firstPage}>
+                1
+              </button>
+
+              <button className='disabled-btn'>...</button>
+            </>
           )}
+
           <p className='current-page'>{this.props.page}</p>
-          {this.props.page === 10 ? (
+
+          {this.props.page >= this.props.totalPages ? (
             ""
           ) : (
-            <button className='next-page-btn' onClick={this.props.nextPage}>
-              +
-            </button>
+            <>
+              <button className='disabled-btn'>...</button>
+
+              <button className='last-page-btn' onClick={this.props.lastPage}>
+                {this.props.totalPages}
+              </button>
+              <button className='next-page-btn' onClick={this.props.nextPage}>
+                +
+              </button>
+            </>
           )}
         </div>
       </div>
